734882495aae40582af5deeb1d14a93cbf93b5fa
MatisLabelle <mat48337@gmail.com>
2025-02-06 09:55:36 -0500

    smooth growth

--- fuga/Assets/Script/MyOSC.cs
+++ fuga/Assets/Script/MyOSC.cs
@@ -1,33 +1,38 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using UnityEngine.Rendering;
 using extOSC;
-using UnityEditor;
 using Treegen;
-
 
 public class MyOSC : MonoBehaviour
 {
-    // public TreegenTreeGenerator treegenTreeGenerator;
+    public GameObject arbreTest;
     public extOSC.OSCReceiver oscReceiver;
-
     public Treegen.TreegenTreeGenerator treegenTreeGenerator;
-    public float[] values = new float[3];
 
-    public static float ScaleValue(float value, float inputMin, float inputMax, float outputMin, float outputMax)
-    {
-        return Mathf.Clamp(((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin), outputMin, outputMax);
-    }
+    private float targetLightMath = 0.0010f;  // Valeur initiale pour l'échelle
+    private float currentLightMath = 0.0010f;
+    private float velocityLight = 0f; // Vélocité pour SmoothDamp
+
+    private float targetLeavesMath = 0f;  // Valeur cible pour l'échelle des feuilles
+    private float currentLeavesMath = 0f;
+    private float velocityLeaves = 0f;
+
+    private int targetBranchesMath = 0;  // Valeur cible pour les branches
+    private int currentBranchesMath = 0;
+    private float velocityBranches = 0f;
+
+    private float targetLeavesOffMath = 0f;  // Valeur cible pour l'offset des feuilles
+    private float currentLeavesOffMath = 0f;
+    private float velocityLeavesOff = 0f;
+
+    public float smoothTime = 0.2f; // Plus petit = plus rapide, plus grand = plus fluide
 
     void Start()
     {
         oscReceiver.Bind("/vkb_midi/9/note/36", message => MessageVolume(message));
-        oscReceiver.Bind("/vkb_midi/9/note/37", message => MessageFrequance(message));
-        oscReceiver.Bind("/btn1", message => MessageBtn1(message));
-        oscReceiver.Bind("/btn2", message => MessageBtn2(message));
-        oscReceiver.Bind("/btn3", message => MessageBtn3(message));
+        oscReceiver.Bind("/vkb_midi/9/note/40", message => MessageFrequance(message));
     }
 
     void MessageVolume(OSCMessage oscMessage)
@@ -35,29 +40,19 @@ public class MyOSC : MonoBehaviour
         float value = 0;
 
         if (oscMessage.Values[0].Type == OSCValueType.Int)
-        {
             value = oscMessage.Values[0].IntValue;
-        }
         else if (oscMessage.Values[0].Type == OSCValueType.Float)
-        {
             value = oscMessage.Values[0].FloatValue;
-        }
         else
-        {
             return;
-        }
 
         Debug.Log("Volume: " + value);
-        float lightMath = Mathf.Lerp(0.0010f, 0.0030f, Mathf.InverseLerp(0f, 200f, value));
-        float leavesMath = Mathf.Lerp(0f, 12f, Mathf.InverseLerp(0f, 200f, value));
-        int branchesMath = Mathf.RoundToInt(Mathf.Lerp(0f, 5f, Mathf.InverseLerp(0f, 200f, value)));
-        float leavesOffMath = Mathf.Lerp(0f, 20f, Mathf.InverseLerp(0f, 200f, value));
 
-
-        treegenTreeGenerator.transform.localScale = Vector3.one * lightMath;
-        treegenTreeGenerator.LeavesScale = Vector3.one * leavesMath;
-        treegenTreeGenerator.BranchSegments = branchesMath;
-        treegenTreeGenerator.LeavesOffset = leavesOffMath;
+        // Mettre à jour les valeurs cibles
+        targetLightMath = Mathf.Lerp(0.0010f, 0.0030f, Mathf.InverseLerp(0f, 200f, value));
+        targetLeavesMath = Mathf.Lerp(0f, 12f, Mathf.InverseLerp(0f, 200f, value));
+        targetBranchesMath = Mathf.RoundToInt(Mathf.Lerp(0f, 5f, Mathf.InverseLerp(0f, 200f, value)));
+        targetLeavesOffMath = Mathf.Lerp(0f, 20f, Mathf.InverseLerp(0f, 200f, value));
     }
 
     void MessageFrequance(OSCMessage oscMessage)
@@ -77,79 +72,37 @@ public class MyOSC : MonoBehaviour
             return;
         }
 
-        Debug.Log("Frequence: " + value);
-        float lightMath = Mathf.Lerp(0f, 5f, value / 1000f);
+        Debug.Log("Volume: " + value);
 
-        treegenTreeGenerator.LeavesNoiseForce = lightMath;
+        // Applique un mappage linéaire pour obtenir la valeur du rouge
+        float red = Mathf.InverseLerp(0f, 200f, value);  // Mappe la valeur pour le rouge
+        float green = 0f;  // Laisse la composante verte à 0
+        float blue = 0f;   // Laisse la composante bleue à 0
 
-    }
+        // Crée une couleur avec uniquement du rouge
+        Color dynamicColor = new Color(red, green, blue);
 
-    void MessageBtn1(OSCMessage oscMessage)
-    {
-        float value = 0;
-
-        if (oscMessage.Values[0].Type == OSCValueType.Int)
+        // Change la couleur du matériau des feuilles (index 1)
+        Renderer renderer = arbreTest.GetComponent<Renderer>();
+        if (renderer != null)
         {
-            value = oscMessage.Values[0].IntValue;
+            Material materialLeafs = renderer.materials[1];  // Sélectionne le deuxième matériau (les feuilles)
+            materialLeafs.SetColor("_BaseColor", dynamicColor);  // Utilise _BaseColor pour modifier la couleur
         }
-        else if (oscMessage.Values[0].Type == OSCValueType.Float)
-        {
-            value = oscMessage.Values[0].FloatValue;
-        }
-        else
-        {
-            return;
-        }
-
-        Debug.Log("btn1: " + value);
-        float lightMath = Mathf.Lerp(0f, 5f, value / 1000f);
-
-    }
-
-    void MessageBtn2(OSCMessage oscMessage)
-    {
-        float value = 0;
-
-        if (oscMessage.Values[0].Type == OSCValueType.Int)
-        {
-            value = oscMessage.Values[0].IntValue;
-        }
-        else if (oscMessage.Values[0].Type == OSCValueType.Float)
-        {
-            value = oscMessage.Values[0].FloatValue;
-        }
-        else
-        {
-            return;
-        }
-
-        Debug.Log("btn2: " + value);
-        float lightMath = Mathf.Lerp(0f, 5f, value / 1000f);
-    }
-
-    void MessageBtn3(OSCMessage oscMessage)
-    {
-        float value = 0;
-
-        if (oscMessage.Values[0].Type == OSCValueType.Int)
-        {
-            value = oscMessage.Values[0].IntValue;
-        }
-        else if (oscMessage.Values[0].Type == OSCValueType.Float)
-        {
-            value = oscMessage.Values[0].FloatValue;
-        }
-        else
-        {
-            return;
-        }
-
-        Debug.Log("btn3: " + value);
-        float lightMath = Mathf.Lerp(0f, 5f, value / 1000f);
-
     }
     void Update()
     {
+        // Lissage des valeurs
+        currentLightMath = Mathf.SmoothDamp(currentLightMath, targetLightMath, ref velocityLight, smoothTime);
+        currentLeavesMath = Mathf.SmoothDamp(currentLeavesMath, targetLeavesMath, ref velocityLeaves, smoothTime);
+        currentBranchesMath = Mathf.RoundToInt(Mathf.SmoothDamp(currentBranchesMath, targetBranchesMath, ref velocityBranches, smoothTime));
+        currentLeavesOffMath = Mathf.SmoothDamp(currentLeavesOffMath, targetLeavesOffMath, ref velocityLeavesOff, smoothTime);
+
+        // Appliquer les valeurs lissées
+        treegenTreeGenerator.transform.localScale = new Vector3(currentLightMath, currentLightMath, currentLightMath);
+        treegenTreeGenerator.LeavesScale = Vector3.one * currentLeavesMath;
+        treegenTreeGenerator.BranchSegments = currentBranchesMath;
+        treegenTreeGenerator.LeavesOffset = currentLeavesOffMath;
 
         treegenTreeGenerator.NewGen();
     }
